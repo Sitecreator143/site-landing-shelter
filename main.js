93b634dd56f4b9536173401f522a67d3cb2eefe9
Alexander Nikolaev <69077479+Sitecreator143@users.noreply.github.com>
2020-10-26 21:14:59 +0400

    init: add site files

---
+++ main.js
@@ -0,0 +1,10 @@
+
+/* 
+
+
+function popupOpen() {
+        $('.popup').addClass('popup__active');
+        $('.popup__done').hide();
+        $('.popup__wrap').show();
+        $('body').css('overflow', 'hidden');
+    } */
