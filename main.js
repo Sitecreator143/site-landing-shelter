0aa436573c839fed9bbbee810d64b48e73943752
Alexander Nikolaev <69077479+Sitecreator143@users.noreply.github.com>
2020-10-27 12:46:27 +0400

    feat: add burger

--- main.js
+++ main.js
@@ -1,3 +1,43 @@
+//Burger pets
+const petsBurgerBtn = document.querySelector('[data-pets-burger]')
+const petsNav = document.querySelector('[data-pets-nav]')
+const body = document.querySelector('body')
+const petsShadow = document.querySelector('[data-pets-shadow]')
+
+// Pets menu burger
+let burgerPetsActive = false
+let burgerPetsPush = () => {
+    petsBurgerBtn.addEventListener('click', () => {
+        if (!(burgerPetsActive)) {
+            burgerPetsActive = true
+            body.classList.add('lock')
+            petsBurgerBtn.classList.add('active')
+            petsNav.classList.add('active')
+            petsShadow.classList.add('active')
+        } else {
+            burgerPetsActive = false
+            body.classList.remove('lock')
+            petsBurgerBtn.classList.remove('active')
+            petsNav.classList.remove('active')
+            petsShadow.classList.remove('active')
+        }
+    })
+}
+let shadowPetsPush = () => {
+    petsShadow.addEventListener('click', () => {
+            burgerPetsActive = false
+            body.classList.remove('lock')
+            petsBurgerBtn.classList.remove('active')
+            petsNav.classList.remove('active')
+            petsShadow.classList.remove('active')
+    })
+}
+shadowPetsPush()
+burgerPetsPush()
+
+
+
+
 
 /* 
 
@@ -8,3 +48,6 @@ function popupOpen() {
         $('.popup__wrap').show();
         $('body').css('overflow', 'hidden');
     } */
+
+
+
